9390cb856d4cc686bd6f38def61d4ec15dce8c1a
Chris Kemp <chris.kemp3000@gmail.com>
2022-04-20 20:06:05 +0100

    API changes

--- generators/api-resource/index.js
+++ generators/api-resource/index.js
@@ -8,11 +8,12 @@ class ApiGenerator extends BaseGenerator {
     this._input({ name: "api", type: "list", choices: apis(this.destinationRoot()) });
     this._input({ name: "parent", type: "list", choices: apiResources(this.destinationRoot()) });
     this._input({ name: "resource", type: "input", validate: kebabCase });
-    this._input({ name: "pathpart", type: "input", default: ({ resource }) => toKebabCase(resource)})
+    this._input({ name: "pathpart", type: "input", default: ({ resource }) => resource})
   }
 
   async create_api_resource() {
     let answers = await this._prompt();
+    answers.fullname = [answers.parent, answers.resource].filter(s => s).join('-');
 
     await this.fs.copyTplAsync(
       this.templatePath('**/*.ejs'),
