0597468b4268e256f41cb18182861152dadf513a
Chris Kemp <chris.kemp3000@gmail.com>
2022-06-27 17:32:51 +0100

    Got builders working and added dynamodb policy

--- generators/api-method/templates/python/api/<%=api%>/<%=resource%>/<%=method%>/scripts/build.sh
+++ generators/api-method/templates/python/api/<%=api%>/<%=resource%>/<%=method%>/scripts/build.sh
@@ -2,6 +2,9 @@ rm -rf ../out
 mkdir ../out
 cp -R ../*.py ../out
 rm -r ../out/*_test.py
+if [ ! -d "../venv" ]; then
+    python -m venv ../venv
+fi
 source ../venv/bin/activate
-pip install --target ../out/  -r requirements.txt
+pip install --target ../out/  -r ../requirements.txt
 deactivate
