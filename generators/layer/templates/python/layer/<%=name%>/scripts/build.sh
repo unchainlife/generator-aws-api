f69128c26237e7588ca8f567242c24a79ec316b8
Chris Kemp <chris.kemp3000@gmail.com>
2022-06-29 16:59:04 +0100

    Corrected terraform build

--- generators/layer/templates/python/layer/<%=name%>/scripts/build.sh
+++ generators/layer/templates/python/layer/<%=name%>/scripts/build.sh
@@ -2,11 +2,11 @@ cd ..
 rm -rf ./build
 rm -rf ./out
 if [ ! -d "venv" ]; then
-    python -m venv ../venv
+    python -m venv venv
 fi
 source venv/bin/activate
 pip install -r requirements.txt
 python setup.py install
 mkdir -p out/python
-mv venv/lib out/python
+cp -R venv/lib out/python
 deactivate
