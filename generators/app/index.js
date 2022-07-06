424e986ab1549c965386ea1706562853a5066457
Burendo <burendo@Chris-M1.local>
2022-07-06 09:35:44 +0100

    Got it working with multiple AWS accounts

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,8 +5,12 @@ class AwsApiGenerator extends BaseGenerator {
   constructor(args, opts) {
     super(args, opts);
 
-    this._input({ name: "project", type: "input", validate: kebabCase });
-    this._input({ name: "region", type: "list", choices: ["eu-west-2"]});
+    this._input({ name: "project", type: "input", validate: kebabCase, store: true });
+    this._input({ name: "region", type: "list", choices: ["eu-west-2"], store: true });
+    this._input({ name: "account_id", type: "input", store: true });
+    this._input({ name: "account_alias", type: "input", default: "mgmt", store: true });
+    this._input({ name: "cli_alias", type: "input", default: "mgmt", store: true });
+    this._input({ name: "role", type: "input", store: true });
   }
 
   async core_application() {
