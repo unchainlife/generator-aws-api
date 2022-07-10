d1064aa519c5a19b2d1179ba5fc005da04949b6a
Chris Kemp <chris.kemp3000@gmail.com>
2022-07-11 00:06:09 +0100

    Added assume_ prefix

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,15 +5,18 @@ class AppGenerator extends BaseGenerator {
   constructor(args, opts) {
     super(args, opts);
 
-    this._input({ name: "project",            type: "input",  validate: kebabCase, store: true });
-    this._input({ name: "region",             type: "list",   choices: ["eu-west-2"], store: true });
-    this._input({ name: "account_id",         type: "input" });
-    this._input({ name: "account_aws_alias",  type: "input",  default: "mgmt" });
-    this._input({ name: "account_cli_alias",  type: "input",  default: "mgmt" });
-    this._input({ name: "account_role_name",  type: "input" });
-    this._input({ name: "dev_account_id",     type: "input", message: "dev account (multi-account only)" });
-    this._input({ name: "test_account_id",    type: "input", message: "test account (multi-account only)" });
-    this._input({ name: "prod_account_id",    type: "input", message: "prod account (multi-account only)" });
+    this._input({ name: "project",              type: "input",  validate: kebabCase, store: true });
+    this._input({ name: "region",               type: "list",   choices: ["eu-west-2"], store: true });
+    this._input({ name: "account_id",           type: "input" });
+    this._input({ name: "account_aws_alias",    type: "input",  default: "mgmt" });
+    this._input({ name: "account_cli_alias",    type: "input",  default: "mgmt" });
+    this._input({ name: "account_role_name",    type: "input" });
+    this._input({ name: "assume_dev_account",   type: "input", message: "dev account (multi-account only)", default: ({ account_id }) => account_id });
+    this._input({ name: "assume_dev_role",      type: "input", message: "dev account (multi-account only)", default: ({ account_role_name }) => account_role_name });
+    this._input({ name: "assume_test_account",  type: "input", message: "dev account (multi-account only)", default: ({ account_id }) => account_id });
+    this._input({ name: "assume_test_role",     type: "input", message: "dev account (multi-account only)", default: ({ account_role_name }) => account_role_name });
+    this._input({ name: "assume_prod_account",  type: "input", message: "dev account (multi-account only)", default: ({ account_id }) => account_id });
+    this._input({ name: "assume_prod_role",     type: "input", message: "dev account (multi-account only)", default: ({ account_role_name }) => account_role_name });
   }
 
   async core_application() {
